3c9d7c4f7da68958e4dd95948305193c5817650b
Velmurugan-kl <velmuruganabr@gmail.com>
2023-12-05 22:19:16 +0530

    first commit

--- src/main/java/com/example/todolist/HelloApplication.java
+++ src/main/java/com/example/todolist/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("To-do_desigh.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("TODO List");
+        stage.setTitle("ToDo List");
         stage.setScene(scene);
         stage.show();
     }
